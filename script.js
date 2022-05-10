97e44b2db76653f84764e008bd4fa31c8cd96eea
Elijah Odjegba <elijahodjegba@gmail.com>
2022-05-10 10:18:25 +0100

    Add expended menu bar with javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("expend").style.display = "block";
+  document.getElementById("burger").style.display = "none";
+}
+function closeNav() {
+ document.getElementById("expend").style.display = "none";
+ document.getElementById("burger").style.display = "block";
+
+}
