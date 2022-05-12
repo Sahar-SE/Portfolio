2c88cf6d2c79ca9ff844c6a761956857a307af80
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-05-12 06:49:23 +0430

    Bring changes

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+let menuOn = document.getElementById("expend");
+let menuOff = document.getElementById("burger"); 
+
 function openNav() {
-  document.getElementById("expend").style.display = "block";
-  document.getElementById("burger").style.display = "none";
+  menuOn.classList.add("show");
+  menuOff.classList.add("burgeract");
 }
+
 function closeNav() {
- document.getElementById("expend").style.display = "none";
- document.getElementById("burger").style.display = "block";
+ menuOn.classList.remove("show");
+ menuOff.classList.remove("burgeract");
 
 }
+
+// let popup = document.getElementById("popup");
+// let popup1 = document.getElementById("popup1");
+// let popup2 = document.getElementById("popup2");
+// let popup3 = document.getElementById("popup3");
+
+// function openPopup() {  
+//   popup.classList.add("open-popup");
+// }
+// function openPopup1() {  
+//   popup1.classList.add("open-popup");
+// }
+// function openPopup2() {  
+//   popup2.classList.add("open-popup");
+// }
+// function openPopup3() {  
+//   popup3.classList.add("open-popup");
+// }
+// function closePopup() {  
+//   popup.classList.remove("open-popup");
+// }
+// function closePopup1() {  
+//   popup1.classList.remove("open-popup");
+// }
+// function closePopup2() {  
+//   popup2.classList.remove("open-popup");
+// }
+// function closePopup3() {  
+//   popup3.classList.remove("open-popup");
+// }
