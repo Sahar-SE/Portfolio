b42fb2d71d626b8fe3c20a2997fb2a7da266b506
sameh <sameh080081@gmail.com>
2022-05-13 21:46:23 +0400

    update validation form.

--- js/formValidate.js
+++ js/formValidate.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("contact-form");
 const emailInput = document.getElementById("email");
-const formButton = document.querySelector(".contact-section-button");
+const formButton = document.getElementById("submit");
 
 form.addEventListener("submit", (event) => {
   if (emailInput.value !== emailInput.value.toLowerCase()) {
@@ -10,8 +10,4 @@ form.addEventListener("submit", (event) => {
     formButton.reportValidity();
     event.preventDefault();
   }
-});
-
-emailInput.addEventListener("input", () => {
-  formButton.setCustomValidity("");
 });
