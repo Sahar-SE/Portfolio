720d8e2fd382555bad66e358f75661504ac1bd4d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-12-14 20:02:11 +0430

    Add Hotel Reservation in pop up menu

--- js/popUp.js
+++ js/popUp.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    title: 'Tonic',
+    title: 'Hotel Reservation',
     devs: ['Sahar', ' • backend', ' • 2021'],
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
